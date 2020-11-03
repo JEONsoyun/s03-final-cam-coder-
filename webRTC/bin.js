1f12acf435b7ad3daf7559ce4c0eed8f142f56ab
박지호 <wlgh325@naver.com>
2020-11-03 17:51:27 +0900

    port 변경 및 Dockerize

--- webRTC/bin.js
+++ webRTC/bin.js
@@ -1,6 +1,6 @@
 const debug = require("debug")("bin");
 const app = require('./app');
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 require('./socket.js');
 
 app.listen(port, () => {
