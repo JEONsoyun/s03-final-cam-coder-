c6b6352687e24bb706939a2b2b138e069b467faf
임진희 <jinhee6340@ajou.ac.kr>
2020-11-03 17:46:30 +0900

    frontend

--- frontend/src/pages/mypage/message/detail.vue
+++ frontend/src/pages/mypage/message/detail.vue
@@ -7,7 +7,7 @@
                 <img style="width:120px" src="https://kimstudy.com/Resources/images/tutor.png"/>
             </div>
             <div class="d-flex" style="margin-left:10px;font-size:20px;font-weight:bold;">한국에서 영어 유학</div>
-            <c-button style="height:30px;width:20px;margin-top:90px;margin-left:10px;">쪽지 목록으로 돌아가기</c-button>
+            <c-button style="height:30px;width:20px;margin-top:80px;margin-left:10px;">쪽지 목록으로 돌아가기</c-button>
         </div>
         <div class="d-flex flex-row message">
             <div class="d-flex">
