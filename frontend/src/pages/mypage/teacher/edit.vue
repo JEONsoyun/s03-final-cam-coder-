03927a2a9fb17620769203f050edf57ff97cee88
chaeeun kim <fairy037@naver.com>
2020-11-05 23:10:03 +0900

    teacher edit 퍼블리싱 완료

--- frontend/src/pages/mypage/teacher/edit.vue
+++ frontend/src/pages/mypage/teacher/edit.vue
@@ -1,9 +1,57 @@
 <template>
-    <c-mypage-layout title="선생님 정보 수정"> 
-      <button style="margin-top: 24px" @click="onUpdateTeacher">
-        선생님 정보 수정하기
-      </button>
-    </c-mypage-layout>
+  <c-mypage-layout title="선생님 정보 수정">
+    <div class="mypage-teacher-edit-page">
+      <div class="d-flex align-center mypage-teacher-edit-page__row">
+        <div class="d-flex flex-shrink-0 mypage-teacher-edit-page__label">소개</div>
+        <div class="mypage-teacher-edit-page__textarea-container">
+          <textarea style="width: 100%;height:100px;" v-model="data.intro" />
+        </div>
+      </div>
+      <div class="d-flex align-center mypage-teacher-edit-page__row">
+        <div class="d-flex flex-shrink-0 mypage-teacher-edit-page__label">경력</div>
+        <div class="mypage-teacher-edit-page__textarea-container">
+          <textarea style="width: 100%;height:100px" v-model="data.expertise" />
+        </div>
+      </div>
+      <div class="d-flex align-center mypage-teacher-edit-page__row">
+        <div class="mypage-teacher-edit-page__label">수업료</div>
+        <v-text-field
+          class="mypage-teacher-edit-page-input"
+          v-model="data.price"
+          dense
+          hide-details
+          solo
+          flat
+          outlined
+        ></v-text-field>
+      </div>
+      <!-- <div class="d-flex align-center mypage-teacher-edit-page__row">
+        <div class="mypage-teacher-edit-page__label">프로필 이미지</div>
+        <v-text-field
+          class="mypage-teacher-edit-page-input"
+          v-model="data.profile"
+          dense
+          hide-details
+          solo
+          flat
+          outlined
+        ></v-text-field>
+      </div> -->
+      <div class="d-flex align-center mypage-teacher-edit-page__row">
+        <div class="mypage-teacher-edit-page__label">가능 일자</div>
+        <v-text-field
+          class="mypage-teacher-edit-page-input"
+          v-model="data.avaliableTime"
+          dense
+          hide-details
+          solo
+          flat
+          outlined
+        ></v-text-field>
+      </div>      
+      <c-button class="flex-grow-0" style="margin-top: 60px;" @click="onUpdateTeacher">수정하기</c-button>
+    </div>
+  </c-mypage-layout>
 </template>
 <script>
 export default {
@@ -19,13 +67,13 @@ export default {
   }),
   methods: {
     async onUpdateTeacher() {
-      try {
-        console.log(this.$store.state.config);
-        await this.$api.updateTeacher(this.data, this.$store.state.config);
-        alert('정보변경 성공');
-      } catch (e) {
-        alert('정보변경 실패.');
-      }
+      // try {
+      //   console.log(this.$store.state.config);
+      //   await this.$api.updateTeacher(this.data, this.$store.state.config);
+      //   alert('정보변경 성공');
+      // } catch (e) {
+      //   alert('정보변경 실패.');
+      // }
     },
   },
   async created() {
@@ -33,3 +81,44 @@ export default {
   },
 };
 </script>
+<style>
+.mypage-teacher-edit-page {
+  width: 400px;
+  margin: 0 auto;
+}
+.mypage-teacher-edit-page-input input {
+  padding: 0 !important;
+}
+.mypage-teacher-edit-page-input .v-input__control {
+  height: 42px !important;
+}
+
+.mypage-teacher-edit-page-input .v-text-field__slot {
+  height: 42px;
+  font-size: 14px;
+  color: #8a8a8a !important;
+}
+.mypage-teacher-edit-page-input fieldset {
+  border-color:  #fb8805 !important;
+  height: 46px;
+}
+
+.mypage-teacher-edit-page__row {
+  margin-bottom: 16px;
+}
+
+.mypage-teacher-edit-page__label {
+  margin-right: 16px;
+  width: 104px;
+}
+
+.mypage-teacher-edit-page__textarea-container {
+  width: 100%;
+  padding: 8px;
+  padding-bottom: 2px;
+  border: solid 1px #fb8805;
+  border-radius: 4px;
+  color: #686868;
+  font-size: 14px;
+}
+</style>
