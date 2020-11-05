60d6117e47f4f7b18b81e7118f05b196471e12f6
chaeeun kim <fairy037@naver.com>
2020-11-05 19:11:36 +0900

    teacher search 페이지 퍼블리싱 완료

--- frontend/src/pages/teacher/search.vue
+++ frontend/src/pages/teacher/search.vue
@@ -14,18 +14,35 @@
               label="키워드를 입력해주세요."
               @keydown.enter="onSearchClick"
             ></v-text-field>
-            <c-button class="flex-grow-0" style="width:80px;">검색</c-button>
+            <c-button type="gradient" class="flex-grow-0" style="width:80px;">검색</c-button>
             </div>
-            <div class="teacher-search-page__content">
-                <div class="teacher-search-page__item">
-                    <div class="teacher-search-page__item-image"></div>
-                    <div class="teacher-search-page__item-content">
-                        <div>text1</div>
-                        <div>text2</div>
-                        <div>text3</div>
-                        <div>text4</div>
+            <div class="d-flex teacher-search-page__content">
+              <div v-for="(item, ti) in SAMPLE_DATA" :key="`teacher-${ti}`" class="d-flex flex-column teacher-search-page__item-container">
+                <div class="d-flex flex-column teacher-search-page__item">
+                  <div class="d-flex flex-shrink-0 teacher-search-page__profile-image">{{item.userProfile}}</div>
+                    <div class="d-flex flex-column teacher-search-page__item-content">
+                        <div style="font-weight: 800;">userName {{item.userName}}</div>
+                        <div class="d-flex flex-grow-1 flex-column">
+                          <div>{{item.expertise}}</div>
+                          <div>{{item.avaliableTime}}</div>
+                        </div>
+                        <div class="d-flex teacher-search-page__item-bottom">
+                          <div class="d-flex flex-grow-0 align-center">
+                            <v-icon color="#ff4a62">favorite</v-icon>
+                            {{item.likeCnt}}
+                          </div>
+                          <div class="d-flex" />
+                          <div class="d-flex flex-grow-0 align-center">
+                            <v-icon color="#4e41ff">account_circle</v-icon>
+                            {{item.studentCnt}}
+                          </div>
+                        </div>
                     </div>
                 </div>
+              </div>
+            </div>
+            <div class="teacher-search-page__more">
+              <c-button @click="onMoreClick" type="gradient">더 불러오기</c-button>
             </div>
           </div>
         </div>
@@ -46,29 +63,142 @@ export default {
       profile: '',
       sorttype: null,
     },
+    SAMPLE_DATA : [
+    {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    },
+        {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    },
+        {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    },
+        {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    },
+        {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    },
+        {
+        "teacherCode": 1,
+        "user": {
+            "userCode": 3,
+            "userId": "teacher0",
+            "userPw": "1234",
+            "userName": "야근이싫은개발자",
+            "userProfile": null,
+            "joinDate": "2020-10-27T01:46:43.000+00:00"
+        },
+        "intro": "안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.",
+        "expertise": "Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.",
+        "price": 20000,
+        "profile": null,
+        "likeCnt": 2,
+        "avaliableTime": "주말 모두 가능, 평일 7시 이후 ",
+        "studentCnt": 3
+    }
+]
   }),
   methods: {
     async onSearchClick() {
-      try {
-        this.teachers = await this.$api.searchTeacher(this.searchKey);
-      } catch (e) {
-        console.log('선생님 로딩 실패');
-      }
+      // try {
+      //   this.teachers = await this.$api.searchTeacher(this.searchKey);
+      // } catch (e) {
+      //   console.log('선생님 로딩 실패');
+      // }
     },
+    onMoreClick() {
+      alert('더 불러올 데이터가 없습니다.');
+    }
   },
   async created() {
-    try {
-      this.teachers = await this.$api.searchTeacher(this.searchKey);
-    } catch (e) {
-      console.log('선생님 로딩 실패');
-    }
+    // try {
+    //   this.teachers = await this.$api.searchTeacher(this.searchKey);
+    // } catch (e) {
+    //   console.log('선생님 로딩 실패');
+    // }
   },
 };
 </script>
 
 <style>
 .teacher-search-page__container {
-  padding: 40px 0;
+  padding: 40px 0 0 0;
   width: 100%;
 }
 
@@ -78,6 +208,9 @@ export default {
 }
 
 .teacher-search-page__title {
+  color: #525252;
+  font-size: 18px;
+  font-weight: 600;
 }
 
 .teacher-search-page__search-container {
@@ -100,5 +233,51 @@ export default {
 .teacher-search-page-input fieldset {
   border-color:  #fb8805 !important;
   height: 46px;
+}
+
+.teacher-search-page__content {
+  margin-top: 40px;
+  flex-wrap: wrap;
+}
+
+.teacher-search-page__item-container {
+  width: 25%;
+}
+
+.teacher-search-page__item {
+  margin: 24px 12px;
+  height: 350px;
+  cursor: pointer;
+  border: solid 1px #fb8805;
+  border-radius: 8px;
+  background: #fff;
+  overflow: hidden;
+}
+
+.teacher-search-page__item:hover {
+  box-shadow: 0 0 20px 4px rgb(0,0,0,0.1);
+  transition: all 0.3s;
+}
+
+.teacher-search-page__profile-image {
+  width: 100%;
+  height: 180px;
+  background: #dfdfdf;
+}
+
+.teacher-search-page__item-content {
+  height: 100%;
+  padding: 12px 8px;
+  font-size: 14px;
+  letter-spacing: -0.38px;
+}
+
+.teacher-search-page__item-bottom {
+  margin-top: 8px;
+  font-weight: 800;
+}
+
+.teacher-search-page__more {
+  margin-top: 40px;
 }
 </style>
