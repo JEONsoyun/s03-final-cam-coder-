bc4ec4f4e02b316324552491bd81b89f3ba641aa
chaeeun kim <fairy037@naver.com>
2020-11-10 15:05:55 +0900

    teacher detail 퍼블리싱 완료

--- frontend/src/pages/teacher/detail.vue
+++ frontend/src/pages/teacher/detail.vue
@@ -8,9 +8,13 @@
             <div
               class="d-flex flex-column flex-grow-0 flex-shrink-0 teacher-detail-page__right-box"
             >
-              <div class="d-flex flex-column flex-grow-1 justify-center align-center">
+              <div
+                class="d-flex flex-column flex-grow-1 justify-center align-center"
+              >
                 <div>
-                  <span style="color: #fb8805;font-weight:800">{{ SAMPLE_DATA.user.userName }}</span>
+                  <span style="color: #fb8805; font-weight: 800">{{
+                    SAMPLE_DATA.user.userName
+                  }}</span>
                   <span>선생님에게</span>
                 </div>
                 <div>원하는 수업에 대해 문의해보세요!</div>
@@ -20,15 +24,50 @@
           </div>
         </div>
         <div class="d-flex teacher-detail-page__box">
-          <div><img :src="SAMPLE_DATA.profile" /></div>
-          <div>
-            <div>{{ SAMPLE_DATA.user.userName }}</div>
-            <div v-html="SAMPLE_DATA.intro"></div>
-            <div>{{ SAMPLE_DATA.expertise }}</div>
-            <div>{{ SAMPLE_DATA.avaliableTime }}</div>
-            <div>like {{ SAMPLE_DATA.likeCnt }}</div>
-            <div>student {{ SAMPLE_DATA.studentCnt }}</div>
+          <div
+            class="d-flex flex-grow-0 flex-shrink-0 teacher-detail-page__profile"
+            :style="`background-image:url(${SAMPLE_DATA.profile})`"
+          />
+          <div class="d-flex flex-column teacher-detail-page__intro">
+            <div class="d-flex">
+              <div style="font-weight: 800; font-size: 24px">
+                {{ SAMPLE_DATA.user.userName }}
+              </div>
+              <div class="d-flex" />
+              <div @click="onFavoriteClick" style="cursor:pointer">
+                <v-icon v-if="isSelected" size="36" color="#fb8805"
+                  >favorite</v-icon
+                >
+                <v-icon v-else size="36" color="#fb8805"
+                  >favorite_border</v-icon
+                >
+              </div>
+            </div>
+            <div class="d-flex">{{ SAMPLE_DATA.expertise }}</div>
+            <div class="d-flex">{{ SAMPLE_DATA.avaliableTime }}</div>
+            <div class="d-flex flex-grow-0" style="font-weight: 800">
+              <div class="d-flex flex-grow-0 align-center">
+                <v-icon color="#ff4a62" style="margin-right: 2px"
+                  >favorite</v-icon
+                >
+                {{ SAMPLE_DATA.likeCnt }}
+              </div>
+              <div class="d-flex" />
+              <div class="d-flex flex-grow-0 align-center">
+                <v-icon color="#4e41ff" style="margin-right: 2px"
+                  >account_circle</v-icon
+                >
+                {{ SAMPLE_DATA.studentCnt }}
+              </div>
+            </div>
           </div>
+        </div>
+        <div class="teacher-detail-page__content">
+          <div style="font-weight: 800; font-size: 18px">선생님 소개</div>
+          <div
+            class="teacher-detail-page__content-box"
+            v-html="SAMPLE_DATA.intro"
+          ></div>
         </div>
       </div>
     </div>
@@ -41,6 +80,7 @@ export default {
   data: () => ({
     teacherId: 0,
     teacher: {},
+    isSelected: false,
     SAMPLE_DATA: {
       teacherCode: 1,
       user: {
@@ -62,7 +102,11 @@ export default {
       studentCnt: 3,
     },
   }),
-  methods: {},
+  methods: {
+    onFavoriteClick() {
+      this.isSelected = !this.isSelected;
+    }
+  },
   async created() {
     this.teacherId = this.$route.params.id;
     console.log(this.teacherId);
@@ -97,6 +141,17 @@ export default {
 .teacher-detail-page__box {
   width: 100%;
   height: 246px;
+}
+
+.teacher-detail-page__profile {
+  width: 246px;
+  height: 100%;
+  background-size: cover;
+  background-position: center center;
+}
+
+.teacher-detail-page__intro {
+  padding: 32px;
   border: solid 1px #eaeaea;
 }
 
@@ -125,5 +180,16 @@ export default {
   border: solid 1px #eaeaea;
   z-index: 2;
   background: #fff;
+}
+
+.teacher-detail-page__content {
+  padding-top: 40px;
+  letter-spacing: -0.38px;
+}
+
+.teacher-detail-page__content-box {
+  margin-top: 16px;
+  padding: 32px;
+  background: rgb(250, 250, 250);
 }
 </style>
