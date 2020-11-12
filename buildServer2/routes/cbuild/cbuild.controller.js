17a1cef3cab5ba7f9a36d34867d40a3e2e460be3
Ubuntu <ubuntu@ip-172-26-7-12.ap-northeast-2.compute.internal>
2020-11-12 15:13:38 +0900

    컴파일 결과 받기 성공(dockerode)

--- buildServer2/routes/cbuild/cbuild.controller.js
+++ buildServer2/routes/cbuild/cbuild.controller.js
@@ -4,56 +4,45 @@ var Docker = require('dockerode');
 var socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
 var docker = new Docker({ socketPath: socket});
 
-function runExec(container) {
+var options = {
+	'Volumes': {
+		'/home/test/': {}
+	},
+	'HostConfig': {
+		'Binds': ['/home/ubuntu/test:/home/test']
+	},
+    'AttachStdout': true,
+	'AttachStderr': true
+};
 
-  var options = {
-    Cmd: ['bash', '-c', 'echo test $VAR'],
-    Env: ['VAR=ttslkfjsdalkfj'],
-    AttachStdout: true,
-    AttachStderr: true
-  };
+exports.test = (req, res, next) => {
+//var code = req.body.code;   // client에서 보낸 코드
+//const source = code.split(/\r\n|\r\n/).join("\n");    // 개행 처리
+//var errData = '';
+//const date = Date.now();
+//const file = date + '.c';
+//var removeFileList = [];
 
-  container.exec(options, function(err, exec) {
-    if (err) return;
-    exec.start(function(err, stream) {
-      if (err) return;
-
-      container.modem.demuxStream(stream, process.stdout, process.stderr);
-
-      exec.inspect(function(err, data) {
-        if (err) return;
-        console.log(data);
-      });
-    });
-  });
+    // 파일로 코드 저장
+    //fs.writeFile(file,source,'utf8',function(error) {
+    //    debug("make file: " + file);
+    //});
+    console.log("hello get /cbuild");
+	// 이미지를 바탕으로 컨테이너를 띄우고
+	// 파일로 저장하고 그 파일을 container와 연결한다!!
+	docker.run('gcc:latest', ['sh', '-c', 'gcc -o myapp /home/test/test.c && ./myapp'], process.stdout, options).then( (data) => {
+		var output = data[0];
+		var container = data[1];
+		console.log("statuscode: ", output.StatusCode);
+		console.log("output: ", output);
+		return container.remove();
+	}).then( (data) => {
+		console.log('container removed');
+	}).catch( (err) => {
+		console.log("Err: ", err);
+	});
 }
 
 exports.build = (req, res, next) => {
-    var code = req.body.code;   // client에서 보낸 코드
-    const source = code.split(/\r\n|\r\n/).join("\n");    // 개행 처리
-    var errData = '';
-    const date = Date.now();
-    const file = date + '.c';
-    var removeFileList = [];
-
-    // 파일로 코드 저장
-    fs.writeFile(file,source,'utf8',function(error) {
-        debug("make file: " + file);
-    });
-
-    docker.createContainer({
-        Image: 'Ubuntu',
-        Tty: true,
-        Cmd: ['/bin/bash'],
-        Volumes: {},
-        VolumesFrom: []
-    }, (err, container) => {
-       container.start({}, (err, data) => {
-           runExec(container);
-       }) 
-    });
-}
-
-exports.test = (req, res, next) => {
     return res.status(200).send('Arrived');
 }
