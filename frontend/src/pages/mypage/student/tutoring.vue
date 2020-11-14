34a97cb12cfddbe87a25098be3aaa8f6ddb3c8b1
chaeeun kim <fairy037@naver.com>
2020-11-14 17:10:31 +0900

    tutoring button interface

--- frontend/src/pages/mypage/student/tutoring.vue
+++ frontend/src/pages/mypage/student/tutoring.vue
@@ -59,6 +59,7 @@
           <div class="d-flex">
             <div class="d-flex" />
             <c-button
+              @click="onStateClick(item)"
               v-if="getType(item)"
               :type="getType(item)"
               class="flex-grow-0"
@@ -76,148 +77,6 @@ export default {
   data: () => ({
     user: {},
     tutorings: [],
-    SAMPLE_DATA: [
-      {
-        id: 1,
-        tutoringCode: 7,
-        teacher: {
-          id: 2,
-          teacherCode: 1,
-          user: {
-            id: 3,
-            userCode: 3,
-            userId: 'teacher0',
-            userPw: '1234',
-            userName: '야근이싫은개발자',
-            userProfile: null,
-            joinDate: '2020-10-27T01:46:43.000+00:00',
-          },
-          intro:
-            '안녕하세요.<br> 저는 위메프에서 웹개발자로 일하고 있는 야근이 싫은 개발자입니다. <br>자바를 처음 접하는\n 분들에게 적합한 커리큘럼입니다. 마지막에는 프로젝트를 완성시켜서 포트폴리오를 하나 만드는 것이 목표입니다. 과외생 분이 배우시는 속도에 따라 커리큘럼은 조금씩 변동될 수 있습니다.<br>현업에서 웹 디자이너와 가장 가깝게 일하는 실무자로서 더 실용적이고 실무에서 필요하는 것들 위주로 가르쳐드리겠습니다.',
-          expertise:
-            'Javascript, Swift, Objective-C, C++, SQL, Java, 프로그래밍 과목을 수업합니다.',
-          price: 20000,
-          profile: '/static/images/user.png',
-          likeCnt: 1,
-          avaliableTime: '주말 모두 가능, 평일 7시 이후 ',
-          studentCnt: 1,
-        },
-        student: {
-          id: 4,
-          userCode: 16,
-          userId: 'studenta',
-          userPw:
-            '$2a$10$q4esBwQfUamOVv4kFCkSiuXdBCEAJeNWwzpFRUJ/o39x828XeUxEG',
-          userName: 'namea',
-          userProfile: 'i like math',
-          joinDate: '2020-10-29T01:07:45.000+00:00',
-        },
-        startDate: '2012-04-23 18:25:44',
-        endDate: '2013-04-23 18:25:44',
-        status: 0,
-        roomNum: 1,
-        requestDate: '2020-10-30T09:31:39.000+00:00',
-      },
-      {
-        id: 5,
-        tutoringCode: 8,
-        teacher: {
-          id: 6,
-          teacherCode: 2,
-          user: {
-            id: 7,
-            userCode: 6,
-            userId: 'teacher1',
-            userPw: '1234',
-            userName: '뭘또이런걸다',
-            userProfile: null,
-            joinDate: '2020-10-27T02:18:23.000+00:00',
-          },
-          intro:
-            '현직 6년차 대리 개발자 입니다. ^^. 컴퓨터 공학과에서 배우는 개론부터 실무가지 전반적인 지식을 정리할 수 있습니다. 개념적인 구조와 역사<br> 또는, C계열이나 Java계열(Android포함), Arduino같은 마이크로콘트롤러까지 다양한 분야를 포괄합니다.',
-          expertise:
-            'C,C++,컴퓨터공학, C#, Java, 프로그래밍 과목을 수업합니다.',
-          price: 20000,
-          profile: '/static/images/user.png',
-          likeCnt: 0,
-          avaliableTime:
-            '현직 개발자이므로 근무시간 외 가능합니다. <br> 평일과 오후6시 이후와 주말 상시입니다.',
-          studentCnt: 1,
-        },
-        student: 4,
-        startDate: '2020-10-23 18:25:44',
-        endDate: '2020-11-23 18:25:44',
-        status: 1,
-        roomNum: 2,
-        requestDate: '2020-10-30T09:34:02.000+00:00',
-      },
-      {
-        id: 5,
-        tutoringCode: 8,
-        teacher: {
-          id: 6,
-          teacherCode: 2,
-          user: {
-            id: 7,
-            userCode: 6,
-            userId: 'teacher1',
-            userPw: '1234',
-            userName: '뭘또이런걸다',
-            userProfile: null,
-            joinDate: '2020-10-27T02:18:23.000+00:00',
-          },
-          intro:
-            '현직 6년차 대리 개발자 입니다. ^^. 컴퓨터 공학과에서 배우는 개론부터 실무가지 전반적인 지식을 정리할 수 있습니다. 개념적인 구조와 역사<br> 또는, C계열이나 Java계열(Android포함), Arduino같은 마이크로콘트롤러까지 다양한 분야를 포괄합니다.',
-          expertise:
-            'C,C++,컴퓨터공학, C#, Java, 프로그래밍 과목을 수업합니다.',
-          price: 20000,
-          profile: '/static/images/user.png',
-          likeCnt: 0,
-          avaliableTime:
-            '현직 개발자이므로 근무시간 외 가능합니다. <br> 평일과 오후6시 이후와 주말 상시입니다.',
-          studentCnt: 1,
-        },
-        student: 4,
-        startDate: '2020-10-23 18:25:44',
-        endDate: '2020-11-23 18:25:44',
-        status: 2,
-        roomNum: 2,
-        requestDate: '2020-10-30T09:34:02.000+00:00',
-      },
-      {
-        id: 5,
-        tutoringCode: 8,
-        teacher: {
-          id: 6,
-          teacherCode: 2,
-          user: {
-            id: 7,
-            userCode: 6,
-            userId: 'teacher1',
-            userPw: '1234',
-            userName: '뭘또이런걸다',
-            userProfile: null,
-            joinDate: '2020-10-27T02:18:23.000+00:00',
-          },
-          intro:
-            '현직 6년차 대리 개발자 입니다. ^^. 컴퓨터 공학과에서 배우는 개론부터 실무가지 전반적인 지식을 정리할 수 있습니다. 개념적인 구조와 역사<br> 또는, C계열이나 Java계열(Android포함), Arduino같은 마이크로콘트롤러까지 다양한 분야를 포괄합니다.',
-          expertise:
-            'C,C++,컴퓨터공학, C#, Java, 프로그래밍 과목을 수업합니다.',
-          price: 20000,
-          profile: '/static/images/user.png',
-          likeCnt: 0,
-          avaliableTime:
-            '현직 개발자이므로 근무시간 외 가능합니다. <br> 평일과 오후6시 이후와 주말 상시입니다.',
-          studentCnt: 1,
-        },
-        student: 4,
-        startDate: '2020-10-23 18:25:44',
-        endDate: '2020-11-23 18:25:44',
-        status: 3,
-        roomNum: 2,
-        requestDate: '2020-10-30T09:34:02.000+00:00',
-      },
-    ],
   }),
   methods: {
     getType(item) {
@@ -229,6 +88,14 @@ export default {
         return 'enter';
       } else {
         return '';
+      }
+    },
+    onStateClick(item) {
+      if (item.status == 1) {
+        console.log('나는 취소하기다.')
+        // TODO: 취소하는 api
+      } else if (item.status == 0) {
+        // TODO: 방 입장하는 기능
       }
     },
   },
